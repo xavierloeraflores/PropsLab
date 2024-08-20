46f3eaf13f5765829f947064288c377cd800f475
Xavier Loera Flores <x.loera.tech@gmail.com>
2024-08-20 03:52:53 -0700

    implemented Features Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import { Hero } from "~/components/hero";
+import { FeatureSectionCollection } from "~/components/feature-section/feature-section-collection";
+import { FeatureSectionCollectionData } from "~/data/features";
+
 export default function HomePage() {
   return (
     <>
       <Hero />
-      Hello World
+      <FeatureSectionCollection {...FeatureSectionCollectionData} />
     </>
   );
 }
