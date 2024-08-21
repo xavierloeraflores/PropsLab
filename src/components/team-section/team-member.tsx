220d5f16eb1c9d9f755be0d74251f640ef60e09e
Xavier Loera Flores <x.loera.tech@gmail.com>
2024-08-21 01:25:18 -0700

    Team Member Component close #15

---
+++ src/components/team-section/team-member.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import { cn } from "~/lib/utils";
+
+export type TeamMemberProps = {
+  name: string;
+  imageSrc?: string;
+  title: string;
+  description: string;
+};
+
+export function TeamMember({
+  name,
+  imageSrc = "/static/images/icon/256x256_white.png",
+  title,
+  description,
+  className,
+}: TeamMemberProps & { className?: string }) {
+  return (
+    <div className={cn("grid gap-4 text-center", className)}>
+      <div className="relative mx-auto flex h-[200px] w-[200px] items-center justify-center">
+        <Image
+          src={imageSrc}
+          alt={name}
+          className="rounded-full object-cover"
+        />
+      </div>
+      <div className="space-y-1">
+        <h3 className="text-lg font-semibold">{name}</h3>
+        <p className="text-muted-foreground">{title}</p>
+        <p className="text-sm text-muted-foreground">{description}</p>
+      </div>
+    </div>
+  );
+}
