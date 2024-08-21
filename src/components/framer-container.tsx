346bb0013c5b54a3190c6fad374bd697f367a583
Xavier Loera Flores <x.loera.tech@gmail.com>
2024-08-20 22:37:03 -0700

    framer motion when component is in view close #10

--- src/components/framer-container.tsx
+++ src/components/framer-container.tsx
@@ -1,18 +1,22 @@
 "use client";
-import { motion } from "framer-motion";
+import { motion, useInView } from "framer-motion";
 import { type ReactNode } from "react";
 import { cn } from "~/lib/utils";
+import { useRef } from "react";
 
 type ContainerProps = {
   children: ReactNode;
   className?: string;
 };
 export function DivContainer({ children, className = "" }: ContainerProps) {
+  const containerRef = useRef(null);
+  const isInView = useInView(containerRef, { once: true });
   return (
     <motion.div
+      ref={containerRef}
       className={cn("", className)}
       initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
+      animate={{ opacity: isInView ? 1 : 0 }}
       transition={{ duration: 1.0 }}
     >
       {children}
@@ -20,11 +24,14 @@ export function DivContainer({ children, className = "" }: ContainerProps) {
   );
 }
 export function SectionContainer({ children, className = "" }: ContainerProps) {
+  const containerRef = useRef(null);
+  const isInView = useInView(containerRef, { once: true });
   return (
     <motion.section
+      ref={containerRef}
       className={cn("w-full py-12 md:py-24 lg:py-32", className)}
       initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
+      animate={{ opacity: isInView ? 1 : 0 }}
       transition={{ duration: 1.0 }}
     >
       {children}
