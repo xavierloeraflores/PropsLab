985a81ca2d2e456c0e494d507ddf793d7a963603
xavierloeraflores <x.loera.tech@gmail.com>
2024-10-02 11:20:24 -0700

    Added FullLogo to Header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,14 @@
 import Link from "next/link";
+import { FullLogo } from "./FullLogo";
 
 export const Header: React.FC = () => {
   return (
-    <header className="fixed left-0 right-0 top-0 z-10 flex min-h-16 flex-row items-center justify-between border-b-2 border-accent bg-secondary p-4 px-6 text-secondary-foreground opacity-95 sm:min-h-24">
-      <div>
-        <Link href="/">Home</Link>
+    <header className="fixed left-0 right-0 top-0 z-10 flex min-h-16 flex-row items-center justify-between border-b-2 border-accent bg-secondary p-2 px-6 text-secondary-foreground opacity-95 sm:min-h-24">
+      <div className="">
+        <Link href="/" className=""><FullLogo size={48}/></Link>
       </div>
-      <div className="ml-auto flex flex-row gap-4">
-        <Link href="/team">About the Team</Link>
+      <div className="ml-auto flex flex-row gap-4 p-4">
+        {/* <Link href="/team">About the Team</Link> */}
         <Link href="/join">Join</Link>
       </div>
     </header>
