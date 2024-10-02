9fd33da28215b77d2bdbe2d182272d58bbfcab35
xavierloeraflores <x.loera.tech@gmail.com>
2024-10-02 11:35:12 -0700

    Footer Gambling Warning

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,15 +1,16 @@
 import Link from "next/link";
 import{ PropsLabLogo} from "./PropsLabLogo";
+import { Separator } from "./ui/separator";
 
 export function Footer() {
   return (
-    <footer className="w-full bg-muted py-6 md:py-12">
+    <footer className="w-full bg-muted py-6 md:py-12 flex flex-col justify-between">
       <div className="container flex max-w-7xl flex-col items-center justify-between gap-6 md:flex-row">
         <Link href="/" className="flex items-center gap-2" prefetch={false}>
           <PropsLabLogo />
           <span className="text-lg font-semibold">Props Lab</span>
         </Link>
-        <nav className="flex items-center gap-4 sm:gap-6">
+        <nav className="flex flex-row justify-center items-center gap-4 sm:gap-6">
           <Link
             href="/team"
             className="text-sm font-medium underline-offset-4 hover:underline"
@@ -25,9 +26,14 @@ export function Footer() {
             Join
           </Link>
         </nav>
-        <p className="text-xs text-muted-foreground">
+        <p className="text-sm text-muted-foreground">
           &copy; 2024 Props Lab. All rights reserved.
         </p>
+      </div>
+      <div className="mt-4 px-8 flex flex-col gap-2 items-center">
+        <Separator/>
+        <span className="text-muted-foreground text-xs">This platform is meant for entertainment purposes only.
+        If you or someone you know has a gambling problem and wants help, call 1-800 GAMBLER. PropsLab is intended for adult users only.</span>
       </div>
     </footer>
   );
