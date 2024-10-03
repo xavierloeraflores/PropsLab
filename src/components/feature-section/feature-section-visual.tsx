99d5fba925edb06b45c03075e2e52efaa3176fc7
xavierloeraflores <x.loera.tech@gmail.com>
2024-10-03 08:50:27 -0700

    responsive features

--- src/components/feature-section/feature-section-visual.tsx
+++ src/components/feature-section/feature-section-visual.tsx
@@ -12,7 +12,7 @@ export function FeatureSectionVisual({
   return (
     <div
       className={cn(
-        "flex min-w-fit flex-col items-center justify-center overflow-hidden",
+        "flex w-full max-w-[50vw] flex-col items-center justify-center overflow-hidden",
         className,
       )}
     >
