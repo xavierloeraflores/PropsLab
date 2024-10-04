bdd59af926ca9b4f471be903a4c3fe1adb0a709f
Xavier Loera Flores <x.loera.tech@gmail.com>
2024-10-04 11:20:14 -0700

    Hero Polishing

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Link from "next/link";
 import { SectionContainer } from "./framer-container";
 import Globe from "./ui/globe";
@@ -8,14 +7,14 @@ export function Hero() {
     <SectionContainer>
       <div className="container flex flex-col-reverse gap-10 px-4 md:flex-row md:gap-6 md:px-6">
         <div className="flex flex-1 flex-col items-center justify-center space-y-4">
-          <div className="space-y-2">
+          <div className="space-y-2 text-center md:text-left">
             <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
               Bet <span className="text-primary">Smarter,{"  "}</span>
               <br className="hidden sm:block" />
               Not <span className="text-primary">Harder</span>
               <span className="text-primary">.</span>
             </h1>
-            <p className="max-w-[600px] text-muted-foreground md:text-xl">
+            <p className="light:text-muted-foreground max-w-[600px] text-gray-300 md:text-xl">
               We are a community of sports and betting enthusiasts who use
               cutting-edge technology to predict outcomes and provide valuable
               insights into the world of sports betting.
