b508dde4fadb5c0de057884d9e13fd3b885b9347
xavierloeraflores <x.loera.tech@gmail.com>
2024-10-18 11:16:15 -0700

    Acernity Globe

---
+++ src/components/acernity/WorldGlobe.tsx
@@ -0,0 +1,50 @@
+"use client";
+import React from "react";
+import dynamic from "next/dynamic";
+// https://ui.aceternity.com/components/github-globe
+
+const World = dynamic(() => import("./acernity-globe").then((m) => m.World), {
+  ssr: false,
+});
+
+const sampleArcs = [
+  {
+    order: 1,
+    startLat: -19.885592,
+    startLng: -43.951191,
+    endLat: -22.9068,
+    endLng: -43.1729,
+    arcAlt: 0.1,
+    color: "#06b6d4",
+  },
+];
+
+export function WorldGlobe() {
+  const globeConfig = {
+    pointSize: 4,
+    globeColor: "#062056",
+    showAtmosphere: true,
+    atmosphereColor: "#FFFFFF",
+    atmosphereAltitude: 0.1,
+    emissive: "#062056",
+    emissiveIntensity: 0.1,
+    shininess: 0.9,
+    polygonColor: "rgba(255,255,255,0.7)",
+    ambientLight: "#38bdf8",
+    directionalLeftLight: "#ffffff",
+    directionalTopLight: "#ffffff",
+    pointLight: "#ffffff",
+    arcTime: 1000,
+    arcLength: 0.9,
+    rings: 1,
+    maxRings: 3,
+    initialPosition: { lat: 22.3193, lng: 114.1694 },
+    autoRotate: true,
+    autoRotateSpeed: 0.5,
+  };
+  return (
+    <div className="relative aspect-square h-full w-full">
+      <World data={sampleArcs} globeConfig={globeConfig} />
+    </div>
+  );
+}
