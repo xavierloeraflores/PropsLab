d21e76c9e65dda54578e922ff955d0432843944d
xavierloeraflores <x.loera.tech@gmail.com>
2024-10-18 19:54:56 -0700

    Header color

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,7 +6,7 @@ const teamPageFlag = env.NEXT_PUBLIC_TEAM_PAGE;
 const joinPageFlag = env.NEXT_PUBLIC_JOIN_PAGE;
 export const Header: React.FC = () => {
   return (
-    <header className="fixed left-0 right-0 top-0 z-10 flex min-h-16 flex-row items-center justify-between border-b-2 border-accent bg-secondary p-2 px-6 text-secondary-foreground opacity-95 dark:bg-slate-900 sm:min-h-24">
+    <header className="fixed left-0 right-0 top-0 z-10 flex min-h-16 flex-row items-center justify-between border-b-2 border-accent bg-secondary p-2 px-6 text-secondary-foreground opacity-95 dark:bg-black sm:min-h-24">
       <div className="">
         <Link href="/" className="">
           <FullLogo size={48} />
