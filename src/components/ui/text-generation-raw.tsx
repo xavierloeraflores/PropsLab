675ac1f21785b723bf1de5586bec0a1970e98da4
xavierloeraflores <x.loera.tech@gmail.com>
2024-10-18 20:01:57 -0700

    text-gneration-raw component

---
+++ src/components/ui/text-generation-raw.tsx
@@ -0,0 +1,66 @@
+"use client";
+import { useEffect } from "react";
+import { motion, stagger, useAnimate } from "framer-motion";
+import { cn } from "~/lib/utils";
+
+export const TextGenerateRaw = ({
+  words,
+  className,
+  filter = true,
+  duration = 0.5,
+}: {
+  words: string;
+  className?: string;
+  filter?: boolean;
+  duration?: number;
+}) => {
+  const [scope, animate] = useAnimate();
+  const wordsArray = words.split(" ");
+  useEffect(() => {
+    const animateWords = async () => {
+      await animate(
+        "span",
+        {
+          opacity: 1,
+          filter: filter ? "blur(0px)" : "none",
+        },
+        {
+          duration: duration ? duration : 1,
+          delay: stagger(0.2),
+        },
+      );
+    };
+    void animateWords();
+  }, [scope.current]);
+
+  const renderWords = () => {
+    return (
+      <motion.div ref={scope}>
+        {wordsArray.map((word, idx) => {
+          return (
+            <motion.span
+              key={word + idx}
+              className="text-black opacity-0 dark:text-white"
+              style={{
+                filter: filter ? "blur(10px)" : "none",
+              }}
+            >
+              {word}{" "}
+            </motion.span>
+          );
+        })}
+      </motion.div>
+    );
+  };
+
+  return (
+    <div
+      className={cn(
+        "font-bold leading-snug text-black dark:text-white",
+        className,
+      )}
+    >
+      <div className="mt-4">{renderWords()}</div>
+    </div>
+  );
+};
