7aaa51e3e76bd813277f8296d7639616bbc218f5
xavierloeraflores <x.loera.tech@gmail.com>
2024-10-18 20:02:14 -0700

    Implemented Text Generation raw

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -1,17 +1,21 @@
 import { SectionContainer } from "~/components/framer-container";
 import { TeamCollection } from "~/components/team-section/team-collection";
 import { TeamCollectionData } from "~/data/team";
+import { TextGenerateRaw } from "~/components/ui/text-generation-raw";
 
 export default function AboutTheTeam() {
   return (
     <SectionContainer>
       <div className="container flex flex-col px-4 md:px-6">
         <div className="flex flex-col items-center justify-center space-y-4 text-center">
-          <div className="space-y-2">
+          <div className="space-y-4">
             <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-              Meet Our Team
+              <TextGenerateRaw
+                words="Meet Our Team"
+                className="text-3xl font-bold tracking-tighter sm:text-5xl"
+              />
             </h2>
-            <p className="max-w-[700px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+            <p className="max-w-[700px] text-primary-foreground md:text-lg/relaxed lg:text-base/relaxed xl:text-lg/relaxed">
               We are a passionate team of visionaries, experts, and strategists
               dedicated to using data-driven approaches and rigorous statistical
               analysis to predict outcomes and make informed betting decisions.
