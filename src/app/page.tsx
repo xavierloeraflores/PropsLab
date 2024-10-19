1495f97f58f603adbc3e9b4bf207418eb229c24e
xavierloeraflores <x.loera.tech@gmail.com>
2024-10-18 20:54:13 -0700

    Sports Squares Component & Collection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Hero } from "~/components/hero";
 import { FeatureSectionCollection } from "~/components/feature-section/feature-section-collection";
 import { FeatureSectionCollectionData } from "~/data/features";
+import SportsSquareCollection from "~/components/sports-squares/sports-square-collection";
 
 export default function HomePage() {
   return (
     <>
       <Hero />
+      <SportsSquareCollection />
       <FeatureSectionCollection {...FeatureSectionCollectionData} />
     </>
   );
