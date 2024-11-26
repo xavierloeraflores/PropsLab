00c39bcaf1c3a92eebea431e10de2ef2992ecad5
xavierloeraflores <x.loera.tech@gmail.com>
2024-11-26 14:33:18 -0800

    team page

--- src/data/team.ts
+++ src/data/team.ts
@@ -1,20 +1,67 @@
 import { type TeamCollectionProps } from "~/components/team-section/team-collection";
 import { type TeamMemberProps } from "~/components/team-section/team-member";
 
-const member1: TeamMemberProps = {
+const Ryan: TeamMemberProps = {
   name: "Ryan Tran",
   title: "Professor Props",
   description: "Founder of Props Lab",
-  imageSrc: "/static/images/team/ryan.jpg",
+  imageSrc: "/static/images/team/ryan.jpeg",
   sports: ["Football", "Basketball", "Baseball", "Hockey", "Soccer"],
 };
-const member2: TeamMemberProps = {
-  name: "Team Member",
-  title: "Role",
-  description: "Description",
-  sports: ["Football", "Esports", "Baseball"],
+
+const Collin: TeamMemberProps = {
+  name: "Collin",
+  title: "Analyst",
+  description: "",
+  imageSrc: "/static/images/team/collin.jpeg",
+  sports: ["Esports", "BasketBall", "Esports"],
+};
+const Leech: TeamMemberProps = {
+  name: "sT Leech",
+  title: "Analyst",
+  description: "",
+  imageSrc: "/static/images/team/leech.jpeg",
+  sports: ["Basketball", "Football", "Esports"],
+};
+const JustSamuel: TeamMemberProps = {
+  name: "JustSamuel",
+  title: "Analyst",
+  description: "",
+  imageSrc: "/static/images/team/justsamuel.jpg",
+  sports: ["Esports"],
+};
+const VuuTang: TeamMemberProps = {
+  name: "VuuTang",
+  title: "Analyst",
+  description: "",
+  imageSrc: "/static/images/team/vuutang.jpeg",
+  sports: ["Basketball"],
+};
+
+const Spectade: TeamMemberProps = {
+  name: "Spectade",
+  title: "Analyst",
+  description: "",
+  imageSrc: "/static/images/team/spectade.jpeg",
+  sports: ["Hockey"],
+};
+
+const LockMan: TeamMemberProps = {
+  name: "LockMan",
+  title: "Co-Founder",
+  description: "",
+  imageSrc: "/static/images/team/lockman.jpeg",
+  sports: ["Basketball", "Football", "Baseball"],
+};
+
+const Alex: TeamMemberProps = {
+  name: "Alex",
+  title: "Analyst",
+  description: "",
+  imageSrc: "/static/images/team/alex.jpeg",
+  sports: ["Basketball", "Football", "Hockey", "Baseball"],
 };
 
 export const TeamCollectionData: TeamCollectionProps = {
-  members: [member1, member2, member2, member2, member2, member2, member2],
+  members: [Ryan, LockMan, Collin, Leech, JustSamuel, VuuTang, Spectade, Alex],
 };
