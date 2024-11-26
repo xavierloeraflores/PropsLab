b0b93eb1fb300a4b23944c74bdfd90782a1f031f
xavierloeraflores <x.loera.tech@gmail.com>
2024-11-26 13:21:35 -0800

    Updated Player cards with data & styling

--- src/components/feature-components/feature2.tsx
+++ src/components/feature-components/feature2.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Marquee from "../ui/marquee";
 import { Card } from "../ui/card";
 import { cn } from "~/lib/utils";
@@ -17,6 +16,7 @@ export function Feature2() {
   );
 }
 
+// Images: https://www.espn.com
 const picks: PickCardProps[] = [
   {
     name: "Travis Kelce",
@@ -24,8 +24,31 @@ const picks: PickCardProps[] = [
     image: "/static/images/players/kelce.png",
     moreLess: true,
     strike: 62.5,
-    // category: "Receiving Yards",
     category: "Rec Yards",
+  },
+  {
+    name: "Lionel Messi",
+    league: "MLS",
+    image: "/static/images/players/messi.png",
+    moreLess: false,
+    strike: 52.5,
+    category: "Passes",
+  },
+  {
+    name: "Shohei Ohtani",
+    league: "MLB",
+    image: "/static/images/players/ohtani.png",
+    moreLess: true,
+    strike: 0.5,
+    category: "Walks",
+  },
+  {
+    name: "Stephen Curry",
+    league: "NBA",
+    image: "/static/images/players/curry.png",
+    moreLess: true,
+    strike: 22.5,
+    category: "Points",
   },
   {
     name: "Kirk Cousins",
@@ -33,17 +56,31 @@ const picks: PickCardProps[] = [
     image: "/static/images/players/cousins.png",
     moreLess: false,
     strike: 427.5,
-    // category: "Passing Yards",
     category: "Pass Yards",
   },
   {
     name: "Tyreek Hill",
     league: "NFL",
     image: "/static/images/players/hill.png",
-    moreLess: true,
+    moreLess: false,
     strike: 97.5,
-    // category: "Receiving Yards",
     category: "Rec Yards",
+  },
+  {
+    name: "Cristiano Ronaldo",
+    league: "SPL",
+    image: "/static/images/players/ronaldo.png",
+    moreLess: true,
+    strike: 3,
+    category: "Shots",
+  },
+  {
+    name: "Aaron Judge",
+    league: "MLB",
+    image: "/static/images/players/judge.png",
+    moreLess: true,
+    strike: 0.5,
+    category: "Hits",
   },
 ];
 
@@ -66,6 +103,7 @@ function PickCard({
   category,
   className = "",
 }: PickCardProps) {
+  const strikeColor = moreLess ? "text-green-400" : "text-red-400";
   return (
     <Card
       className={cn("bg-background px-4 py-2 dark:bg-slate-900", className)}
@@ -73,20 +111,24 @@ function PickCard({
       <div className="flex flex-col items-center justify-center gap-2">
         <div className="flex items-center justify-center">
           <Avatar className="h-20 w-24 bg-primary">
-            <AvatarImage src={image} alt={name} />
+            <AvatarImage src={image} alt={name} className="object-cover" />
             <AvatarFallback>{name[0]}</AvatarFallback>
           </Avatar>
         </div>
         <div className="flex flex-col items-center justify-center gap-1">
-          <h2 className="text-xl font-bold text-primary">{name}</h2>
+          <h2 className="text-xl font-bold text-primary-foreground">{name}</h2>
           <p className="text-sm text-gray-300">{league}</p>
         </div>
         <div className="flex h-16 flex-row items-center justify-center gap-1 rounded-lg bg-slate-700 p-2 font-bold text-primary-foreground">
           <p className="text-sm">{category}</p>
           <Separator className="mx-2 h-8 w-0.5 bg-primary-foreground" />
-          <div className="flex flex-col items-center justify-between">
-            <p className="text-lg">{strike}</p>
-            <p className="text-sm">{moreLess ? "More" : "Less"}</p>
+          <div
+            className={
+              "flex flex-col items-center justify-between " + strikeColor
+            }
+          >
+            <p className="text-center text-lg">{strike}</p>
+            <p className="text-center text-sm">{moreLess ? "More" : "Less"}</p>
           </div>
         </div>
       </div>
